10e849f9b969bc7671b4b1e06f1eaa2324191b19
JorshGL <jorge.gl0905@gmail.com>
2022-06-27 22:12:10 -0500

    :hot_face:

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 def create_app():
     app = Flask(__name__)
     import config
     app.config.from_object(config)
+    import auth
+    app.register_blueprint(auth.bp)
+    
+    
+    @app.route('/')
+    def test():
+        storage = PyMongo(app).db.users
+        user = {
+        "name" : "juan",
+        "username" : "equisde",
+        "password" : "ksjadkds"
+        }
+        storage.insert_one(user)
+        return "done"
     return app
 
 
--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,16 +1,39 @@
-from flask import Blueprint, request, abort
+from flask import Blueprint, current_app, request, abort, Response, jsonify
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-bp = Blueprint(__name__, 'auth', url_prefix='auth')
+bp = Blueprint(__name__, 'auth', url_prefix='/auth')
 
 @bp.route('/register', methods=['POST'])
 def register():
-    try:
-        data = request.get_json()
-        username = data.get('username')
-        password = data.get('password')
-    except Exception:
-        abort(400)
+    storage = PyMongo(current_app).db.users
+    data = request.get_json()
+    name = data.get('name')
+    username = data.get('username')
+    password = data.get('password')
+        
+    if not name or not username or not password:
+        response = {
+            'status' : 'error',
+            'message' : "There's no name, no username or no password in request, please, try again."
+        }
+        return Response(jsonify(response), 400)
+    
+    hashed_pass = generate_password_hash(password)  
+    user = {
+        'name' : name,
+        'username' : username,
+        'password' : hashed_pass
+    }
+    storage.insert_one(user)
+    
+    response = {
+        'status' : 'done',
+        'message' : f'user registered as {user}'
+    }
+    return Response(jsonify(response), 200)
+
     
     
     
