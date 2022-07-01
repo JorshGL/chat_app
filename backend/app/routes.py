65be15864828cc82b13731c5850be3e78a972591
JorshGL <jorge.gl0905@gmail.com>
2022-06-30 19:20:19 -0500

    si

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,6 @@
 from auth import login_required
 from flask import Blueprint, current_app, Response
-from json import dumps
+from bson import json_util
 from flask_cors import cross_origin
 
 bp = Blueprint(__name__, 'routes')
@@ -10,5 +10,6 @@ bp = Blueprint(__name__, 'routes')
 @cross_origin()
 def get_users():
     storage = current_app.config['db'].users
-    users = storage.find()
-    return Response(dumps(users), 200, mimetype='application/json')
+    result = storage.find({})
+    users = json_util.dumps(result, indent=2)
+    return Response(users, 200, mimetype='application/json')
