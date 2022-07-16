26140b16451353751fabddf309bef007cfb29bb2
JorshGL <jorge.gl0905@gmail.com>
2022-07-16 16:41:05 -0500

    :angry:

--- backend/app/users.py
+++ backend/app/users.py
@@ -1,13 +1,12 @@
-from auth import login_required, get_data, set_profile_pic
+from auth import login_required, get_data
 from flask import Blueprint, current_app, Response, session, request
-from bson import json_util
+from bson import json_util, ObjectId
 from flask_cors import cross_origin
 import re
 from json import dumps
 
 
-
-bp = Blueprint(__name__, 'routes', url_prefix='/users')
+bp = Blueprint(__name__, 'users', url_prefix='/users')
 
 
 @bp.route('/all')
@@ -44,23 +43,31 @@ def search(user):
 
 
 @bp.route('/me', methods=['GET', 'PATCH'])
+@login_required
 @cross_origin()
 def account():
-    user_id = session.get('user_id')
+    user_id = ObjectId(session.get('user_id'))
+    print(user_id)
     storage = current_app.config['db'].users
     
     if request.method == 'GET':
         user = storage.find_one({'_id' : user_id})
+        print(user)
         return Response(json_util.dumps(user), 
                         200, mimetype='application/json')
     
     if request.method == 'PATCH':
-        name, username, password, profile_pic = get_data('name', 'user', 'pass', 'profile_pic')
-        storage.update_one({'_id': user_id}, {'$set': {'name': name, 'user': user, 'pass': password}})
-        if profile_pic is not None:
-            set_profile_pic(profile_pic, username)
+        name, user, password, profile_pic = get_data('name', 'user', 'pass', 'profile_pic')
+        check = storage.find_one({'user' : user})
+        if check is not None and check.get('_id') != user_id:
+            return Response(dumps({
+                'status' : 'error',
+                'message' : 'username already exists'
+            }), 409, mimetype = 'application/json')
+            
+        storage.update_one({'_id': user_id}, {'$set': {'name': name, 'user': user, 'pass': password, 'profile_pic': profile_pic}})
         return Response(dumps({
             'status' : 'updated',
-            'user' : storage.find_one({'_id' : user_id})
+            'user' : json_util.dumps(storage.find_one({'_id' : user_id}))
         }), 200, mimetype = 'application/json')
         
